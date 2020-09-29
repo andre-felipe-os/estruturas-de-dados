329efe37ad2517f02ccb4cad8f82ed3125bc59f4
André Felipe <andre.lipe.o@gmail.com>
2020-09-29 14:27:14 -0300

    Função "addNode" da lista foi adicionada.

--- list.c
+++ list.c
@@ -9,6 +9,15 @@ struct node {
 
 typedef struct node Node;
 
+void addNode(Node *first, int value) {
+  Node *n = malloc(sizeof(Node));
+  n->value = value;
+  n->proximo = first;
+  n->anterior = first->anterior;
+
+  first->anterior = n;
+}
+
 int main() {
   Node *n1 = malloc(sizeof(Node));
   n1->value = 1;
@@ -19,7 +28,9 @@ int main() {
   n1->proximo = n2;
   n2->anterior = n1;
 
-  printf("%d, %d\n", n1->value, n1->proximo->value);
+  addNode(n1, -6);
+
+  printf("%d, %d, %d\n", n1->anterior->value, n1->value, n1->proximo->value);
 
   free(n1);
   free(n2);
