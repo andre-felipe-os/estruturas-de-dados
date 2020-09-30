118b0ce533fbe0c4e62d7c4368d59423ad149fc3
André Felipe <andre.lipe.o@gmail.com>
2020-09-30 11:26:53 -0300

    Funções "show" e "remove" da lista foram implementadas.

--- list.c
+++ list.c
@@ -12,10 +12,35 @@ typedef struct node Node;
 void addNode(Node *first, int value) {
   Node *n = malloc(sizeof(Node));
   n->value = value;
+  
   n->proximo = first;
   n->anterior = first->anterior;
 
   first->anterior = n;
+  n->anterior->proximo = n;
+}
+
+void removeNode(Node *first, int value) {
+  Node *i = first;
+
+  while (i->value != value) {
+    i = i->proximo;
+  }
+
+  i->anterior->proximo = i->proximo;
+  i->proximo->anterior = i->anterior;
+  free(i);
+}
+
+void showAll(Node *first) {
+  Node *i = first;
+
+  while (i != first->anterior) {
+    printf("%d, ", i->value);
+    i = i->proximo;
+  }
+
+  printf("%d\n", i->value);
 }
 
 int main() {
@@ -26,14 +51,22 @@ int main() {
   n2->value = 4;
 
   n1->proximo = n2;
+  n1->anterior = n2;
+  n2->proximo = n1;
   n2->anterior = n1;
 
   addNode(n1, -6);
+  addNode(n1, -13);
+  addNode(n1, 0);
+  addNode(n1, 2);
+  removeNode(n1, 4);
 
-  printf("%d, %d, %d\n", n1->anterior->value, n1->value, n1->proximo->value);
+  //printf("%d, %d, %d\n", n1->anterior->value, n1->value, n1->proximo->value);
+  showAll(n1);
 
+  /*free(n1->anterior);
   free(n1);
-  free(n2);
+  free(n2);*/
   
   return 0;
 }
